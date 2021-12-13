b088c52bf16b6999e23c1b6ecc0ba346a6fee4b3
Arthur Sallé <arthursalle.pro@gmail.com>
2021-12-13 11:49:23 +0100

    refacto affectation task

--- src/components/task-list.vue
+++ src/components/task-list.vue
@@ -30,7 +30,7 @@
           <v-list class="d-flex">
             <v-list-item-avatar
               class="task--avatar my-0 ml-0 mr-0"
-              v-for="(employee, i) in employeesModel.data.slice(0, 4)"
+              v-for="(employee, i) in getAffectations(task)"
               :key="i"
               size="35"
             >
@@ -66,7 +66,23 @@ export default {
       return this.$store.state.employees
     },
     affectationsModel() {
-      return this.$store.state.tasks
+      return this.$store.state.affectations
+    },
+  },
+
+  methods: {
+    getAffectations(task) {
+      return this.affectationsModel.data
+        .filter((affectation) => {
+          if (affectation.task_id === task.id) {
+            return true
+          }
+        })
+        .map((employee) => {
+          return this.employeesModel.data.find(
+            (e) => e.id === employee.employee_id
+          )
+        })
     },
   },
 }
