7094d11c0e80a2c2741b2322da3545ccbbefac62
Arthur Sallé <arthursalle.pro@gmail.com>
2021-12-13 15:45:49 +0100

    refacto top bar

--- src/components/top-bar.vue
+++ src/components/top-bar.vue
@@ -1,23 +1,41 @@
 <template>
   <v-app-bar
-    id="top-bar"
+    class="topbar px-6"
     color="var(--color-bg-blue)"
     elevate-on-scroll
     fixed
     app
-    class="px-6"
   >
     <v-btn class="pa-2" min-width="0">
       <v-icon color="var(--color-gray)">search</v-icon>
     </v-btn>
 
     <v-spacer></v-spacer>
+    <div class="d-flex align-center">
+      <v-btn class="pa-2" min-width="0">
+        <v-icon color="var(--color-gray)" class="material-icons-outlined">
+          settings
+        </v-icon>
+      </v-btn>
 
-    <v-btn class="pa-2" min-width="0">
-      <v-icon color="var(--color-gray) " class="material-icons-outlined">
-        settings
-      </v-icon>
-    </v-btn>
+      <v-badge top color="var(--color-font)" dot overlap>
+        <v-btn
+          class="pa-2 ml-4 font-weight-bold"
+          color="indigo lighten-4"
+          min-width="0"
+          elevation="0"
+          :ripple="false"
+        >
+          AS
+        </v-btn>
+      </v-badge>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Arthur Sallé</v-list-item-title>
+          <v-list-item-subtitle>arthursalle.pro@gmail.com</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
   </v-app-bar>
 </template>
 
@@ -34,8 +52,8 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#top-bar {
+<style lang="scss" scoped>
+.topbar {
   z-index: 100;
 }
 </style>
